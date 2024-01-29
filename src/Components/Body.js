30df4a7a9b5601cc3130508bd45301e685128037
Roshan-Pawar <roshan03pawar@gmail.com>
2024-01-29 16:57:56 +0530

    Filter feature added using hook's

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,18 +1,56 @@
 import ResCard from "./Rescard";
-import resList from "../Utils/Data";
+import resList from "../Utils/data";
+import { useState } from "react";
 
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">Search !!</div>
-        <div className="res-container">
-          {/* This map function will loop over the resList object for every restuarant */}
-          {resList.map((resList) => (
-            <ResCard key={resList.info.id} resList = {resList}/>
-          ))}
-        </div>
+  const [listOfRes, setlistOfRes] = useState(resList);
+
+  return (
+    <div className="body">
+      <div className="search">Search !!</div>
+      <div className="filter">
+        <button
+          className="all-filter"
+          onClick={() => {
+            const allListOfRes = resList;
+            setlistOfRes(allListOfRes);
+          }}
+        >
+          All Restaurants
+        </button>
+
+        <button
+          className="rating-filter"
+          onClick={() => {
+            const topRatedListOfRes = listOfRes.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setlistOfRes(topRatedListOfRes);
+          }}
+        >
+          Ratings 4.0+
+        </button>
+
+        <button
+          className="fast-filter"
+          onClick={() => {
+            const fastListOfRes = listOfRes.filter(
+              (res) => res.info.sla.deliveryTime < 30
+            );
+            setlistOfRes(fastListOfRes);
+          }}
+        >
+          Fast Delivery
+        </button>
       </div>
-    );
-  };
+      <div className="res-container">
+        {/* This map function will loop over the resList object for every restuarant */}
+        {listOfRes.map((resList) => (
+          <ResCard key={resList.info.id} resList={resList} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
