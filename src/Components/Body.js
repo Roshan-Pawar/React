6796d90b58aad274789978c6eb0531ac456319e6
Roshan-Pawar <roshan03pawar@gmail.com>
2024-02-13 18:42:49 +0530

    CORS issue solved

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -13,7 +13,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
     
       const json = await data.json();
       //optional chaining
